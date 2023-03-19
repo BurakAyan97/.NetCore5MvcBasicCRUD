ffc43b70ce5383d0d4c13ebfb131eb7446cbd5a8
BurakAyan97 <burakayan97@outlook.com>
2023-03-19 22:54:50 +0300

    Add project files.

---
+++ CoreDepartman/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreProject.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace CoreProject.Controllers
+{
+    //username : burak , şifre : 123123  (Personeller listesine bakmak isterseniz)
+    public class LoginController : Controller
+    {
+        Context c = new Context();
+
+        [HttpGet]
+        public IActionResult GirisYap()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GirisYap(Admin p)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.Kullanici == p.Kullanici && x.Sifre == p.Sifre);
+            if (bilgiler != null) //kullanıcı değerleri eşleşiyorsa yapılacaklar
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.Kullanici)
+                };
+                var useridentity = new ClaimsIdentity(claims, "Login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Personeller");
+            }
+            return View();
+        }
+    }
+}
---
+++ CoreDepartman/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreProject.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-S2C7UGO;Database=CoreDepartmanDb;User ID=sa;Password=arkadas1");
+        }
+
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+
+    }
+}
